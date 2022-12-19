5b09ae363e882687f0611f7c07b43934895bae71
Jackeybai <1303802862@qq.com>
2022-12-19 17:06:52 +0800

    创建组件验证路由功能

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Home组件</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
