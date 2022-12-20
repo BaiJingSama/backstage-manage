c52c45879df6e094fe998127e13debb7004a7da5
Jackeybai <1303802862@qq.com>
2022-12-20 16:33:07 +0800

    实现点击菜单栏跳转到相应路由的功能

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import VueRouter from "vue-router";
 import Home from "../views/Home";
 import User from "../views/User";
 import Main from "../views/Main";
+import Mall from "../views/Mall";
+import PageOne from "../views/PageOne";
+import PageTwo from "../views/PageTwo";
 
 // 1.创建路由表
 // 2.将路由和组件进行映射
@@ -11,9 +14,13 @@ const routes = [
   {
     path: "/",
     component: Main,
+    redirect: "/home",
     children: [
       { path: "home", component: Home },
       { path: "user", component: User },
+      { path: "mall", component: Mall },
+      { path: "page1", component: PageOne },
+      { path: "page2", component: PageTwo },
     ],
   },
 ];
