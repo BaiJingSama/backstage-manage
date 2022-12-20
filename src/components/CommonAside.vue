8e671fb6a082527c2a8f76ed87a5be16a188add8
Jackeybai <1303802862@qq.com>
2022-12-20 16:31:46 +0800

    实现点击菜单栏跳转到相应路由的功能

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -3,7 +3,7 @@
     <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
       :collapse="isCollapse" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
       <h3>白境的后台管理系统</h3>
-      <el-menu-item v-for="item in noChildren" :key="item.name" :index="item.name">
+      <el-menu-item @click="clickMenu(item)" v-for="item in noChildren" :key="item.name" :index="item.name">
         <i :class="`el-icon-${item.icon}`"></i>
         <span slot="title">{{item.label}}</span>
       </el-menu-item>
@@ -14,7 +14,7 @@
           <span slot="title">{{item.label}}</span>
         </template>
         <el-menu-item-group v-for="subItem in item.children" :key="subItem.path">
-          <el-menu-item :index="subItem.path">{{subItem.label}}</el-menu-item>
+          <el-menu-item @click="clickMenu(subItem)" :index="subItem.path">{{subItem.label}}</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
     </el-menu>
@@ -22,6 +22,8 @@
 </template>
 
 <script>
+import router from '@/router';
+
 export default {
   data() {
     return {
@@ -78,6 +80,11 @@ export default {
     handleClose(key, keyPath) {
       console.log(key, keyPath);
     },
+    clickMenu(item) {
+      if (this.$route.path !== item.path && !(this.$route.path === '/home' && (item.path === '/'))) {
+        router.push(item.path)
+      }
+    }
 
   },
   computed: {
@@ -95,13 +102,18 @@ export default {
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
   min-height: 400px;
+  border: 0;
 }
 
 .el-menu {
   height: 100vh;
 
   h3 {
-    color: #fff
+    color: #fff;
+    text-align: center;
+    line-height: 56px;
+    font-size: 18px;
+    font-weight: 400;
   }
 }
 </style>
