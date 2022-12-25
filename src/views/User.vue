a590d59edcb64b3d2a0edec0d1f1501abdf67d06
Jackeybai <1303802862@qq.com>
2022-12-25 12:02:54 +0800

    新增用户信息的校验和关闭弹窗的重置表单功能实现

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,13 +1,109 @@
 <template>
-  <div>
-    <h1>User组件</h1>
+  <div class="manage">
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="50%" :before-close="handleClose">
+      <!-- 用户的表单信息 -->
+
+      <el-form ref="form" :rules="rules" :inline="true" :model="form" label-width="80px">
+        <el-form-item label="姓名" prop="name">
+          <el-input placeholder="请输入姓名" v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="年龄" prop="age">
+          <el-input placeholder="请输入年龄" v-model="form.age"></el-input>
+        </el-form-item>
+        <el-form-item label="性别" prop="sex">
+          <el-select v-model="form.sex" placeholder="请选择性别">
+            <el-option label="男" value="1"></el-option>
+            <el-option label="女" value="0"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="出生日期" prop="birth">
+          <el-date-picker v-model="form.birth" type="date" placeholder="选择日期" />
+        </el-form-item>
+        <el-form-item label="地址" prop="addr">
+          <el-input placeholder="请输入地址" v-model="form.addr"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="cancel">取 消</el-button>
+        <el-button type="primary" @click="submit">确 定</el-button>
+      </span>
+
+    </el-dialog>
+    <div class="manage-header">
+      <el-button type="primary" @click="dialogVisible = true">+ 新增</el-button>
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="date" label="日期" width="180">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="180">
+        </el-table-column>
+        <el-table-column prop="address" label="地址">
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      dialogVisible: false,
+      form: {
+        name: '',
+        age: '',
+        sex: '',
+        birth: '',
+        addr: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入姓名' },
+          { pattern: /[\u4e00-\u9fa5]/, message: '请填写正确的姓名', trigger: 'blur' }
+        ],
+        age: [
+          { required: true, message: '请输入年龄' },
+          { pattern: /^(?:[1-9][0-9]?|1[01][0-9]|120)$/, message: '请填写正确的年龄', trigger: 'blur' }
+        ],
+        sex: [
+          { required: true, message: '请选择性别' }
+        ],
+        birth: [
+          { required: true, message: '请选择出生日期' }
+        ],
+        addr: [
+          { required: true, message: '请填写地址' }
+        ]
+      },
+      tableData:[
+        
+      ]
+    };
+  },
+  methods: {
+    // 提交用户表单
+    submit() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          // 后续对表单数据处理
+          console.log(this.form);
+          this.$refs.form.resetFields()
+          // 关闭弹窗
+          this.dialogVisible = false
 
-}
+        }
+      })
+    },
+    // 弹窗关闭之前触发该方法
+    handleClose() {
+      this.$refs.form.resetFields()
+      this.dialogVisible = false
+    },
+    // 点击取消的时候触发该方法
+    cancel() {
+      this.handleClose()
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
