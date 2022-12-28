bb04445f3f70584ca754b2a4408e129ae3dd45ee
Jackeybai <1303802862@qq.com>
2022-12-28 16:30:31 +0800

    添加了404页面

--- src/views/PageTwo.vue
+++ src/views/PageTwo.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <h1>PageTwo页面</h1>
+  <div bgcolor="black" class="container">
+    <div class="container9">
+      <h1>404 WARNING</h1>
+      <div class="txt">
+        <p>嗯...本页面也在建设中</p>
+        <p>请再一次谅解</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +16,48 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="less" scoped>
+.container {
+  list-style: none;
+  font-family: 'Noto Sans TC', sans-serif;
+  margin: 220px 30px;
+  background-color: #999;
 
+  .container9 {
+    width: 1300px;
+    margin: auto;
+    text-align: center;
+  }
+
+  .container9 h1 {
+    width: 50%;
+    display: inline-block;
+    margin-top: 50px;
+    box-sizing: border-box;
+    font-size: 80px;
+    background-color: #a00;
+    color: #fff;
+    margin-bottom: 0.5em;
+
+
+  }
+
+  .container9 .txt {
+    flex-shrink: 0;
+    box-sizing: border-box;
+    color: #fff;
+  }
+
+  .container9 .txt P {
+    display: inline-block;
+    margin-bottom: 30px;
+    font-size: 26px;
+    padding: 0 100px;
+  }
+
+  .container9 .txt p:first-child:first-letter {
+    font-size: 40px;
+  }
+
+}
 </style>
